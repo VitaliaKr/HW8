d0e82adbe53a4636b28517a8d458c1fac2359ef8
VitaliaKr <81364950+VitaliaKr@users.noreply.github.com>
2025-03-23 01:47:49 +0400

    done

--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут
+        int[] peopleWeight = new int[5];
+        for (int i = 0; i < peopleWeight.length; i++) {
+            peopleWeight[i] = new Random().nextInt(60) + 40;
+            System.out.println(peopleWeight[i]);
+        }
+        double sumWeight = 0;
+        int weightCount = 0;
+        for (int i = 0; i < peopleWeight.length; i++) {
+            sumWeight += peopleWeight[i];
+        }
+        for (int i = 0; i < peopleWeight.length; i++) {
+            if (peopleWeight[i] > 40 && peopleWeight[i] < 80) {
+                weightCount++;
+            }
+        }
+
+        System.out.println("Средний вес: " + sumWeight / peopleWeight.length);
+        System.out.println("Кол-во людей от 40 до 80: " + weightCount);
     }
 }
